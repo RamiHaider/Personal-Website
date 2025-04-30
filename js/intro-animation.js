4ae9aa546a8a88d22b646115e29de946d67eb2a5
GeomindAI <rami@geomindai.com>
2025-04-30 07:15:33 -0400

    good

--- js/intro-animation.js
+++ js/intro-animation.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const titles = [
-        "Data Scientist",
-        "Data Geoscientist",
         "Machine Learning Engineer",
         "Fullstack Developer",
-        "Professional Geologist"
+        "Data Scientist"
     ];
     
     const dynamicTitleElement = document.getElementById('dynamic-title');
@@ -21,65 +19,15 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Determine if we're adding or removing characters
         if (isDeleting) {
-            // Special handling for Data Geoscientist when deleting
-            if (currentIndex === 1) {
-                if (charIndex > 8) {
-                    // If we're deleting after "Geo", keep the styling
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>Geo</span>" + titleToUse.substring(8, charIndex - 1);
-                } else if (charIndex === 8) {
-                    // When "scientist" is completely deleted
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>Geo</span>";
-                } else if (charIndex === 7) {
-                    // When deleting "Geo" -> "Ge"
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>Ge</span>";
-                } else if (charIndex === 6) {
-                    // When deleting "Ge" -> "G"
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>G</span>";
-                } else if (charIndex === 5) {
-                    // When deleting "G" (moving to just "Data ")
-                    text = "Data ";
-                } else {
-                    // Regular deletion for "Data "
-                    text = text.substring(0, text.length - 1);
-                }
-            } else {
-                // Regular deletion for other titles
-                text = text.substring(0, text.length - 1);
-            }
+            text = titleToUse.substring(0, text.length - 1);
             charIndex--;
         } else {
-            // Add a character
-            if (currentIndex === 1 && charIndex >= 5 && charIndex <= 7) {
-                // For "Geo" in "Data Geoscientist", handle specially
-                const nextChar = titleToUse.charAt(charIndex);
-                
-                // If we're adding a character from "Geo", style it
-                if (charIndex === 5) {
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>G</span>";
-                } else if (charIndex === 6) {
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>Ge</span>";
-                } else if (charIndex === 7) {
-                    text = text.substring(0, 5) + "<span style='color:#A4A155;'>Geo</span>";
-                }
-            } else if (currentIndex === 1 && charIndex > 7) {
-                // For letters after "Geo", preserve the styling
-                const nextChar = titleToUse.charAt(charIndex);
-                text = text.substring(0, 5) + "<span style='color:#A4A155;'>Geo</span>" + titleToUse.substring(8, charIndex + 1);
-            } else {
-                // Regular typing for other parts
-                text = titleToUse.substring(0, charIndex + 1);
-            }
+            text = titleToUse.substring(0, charIndex + 1);
             charIndex++;
         }
         
         // Update the display text
-        if (currentIndex === 1) {
-            // For "Data Geoscientist" with special styling
-            dynamicTitleElement.innerHTML = text;
-        } else {
-            // For regular text
-            dynamicTitleElement.textContent = text;
-        }
+        dynamicTitleElement.textContent = text;
         
         // Calculate typing speed with randomization for realism
         let typeSpeed = isDeleting ? 30 : 80; // Faster overall
